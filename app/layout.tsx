d72342853a6d3dd3c26dc2fbd91fb42d7baf4644
TheSoftNode <thesoftnode@gmail.com>
2024-12-13 10:48:55 +0100

    Add hitoai icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import Footer from "@/components/Footer/Footer";
-import EasmarkNavbar from "@/components/Landing/EasmarkNavbar";
+// import EasmarkNavbar from "@/components/Landing/EasmarkNavbar";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
